74bcf2f87784a1ff44f2c4d670baad263fe0ddf6
jlrussin <jlrussin@gmail.com>
2019-06-03 14:28:50 -0700

    initial commit

---
+++ ConvLSTM.py
@@ -0,0 +1,8 @@
+"""
+2d convolutional lstm as described in the paper:
+Title: Convolutional LSTM Network: A Machine Learning Approach for Precipitation
+       Nowcasting
+Authors: Xingjian Shi, Zhourong Chen, Hao Wang, Dit-Yan Yeung, Wai-kin Wong,
+         Wang-chun Woo
+arxiv: https://arxiv.org/abs/1506.04214
+"""
