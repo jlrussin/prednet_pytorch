3e5e849636e302dd0706eee995a99bf941f96813
jlrussin <jlrussin@gmail.com>
2019-06-08 14:58:48 -0700

    Script for testing prednet with defaults on kitti

---
+++ scripts/train_prednet_kitti_defaults.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#SBATCH -p local
+#SBATCH -A ecortex
+#SBATCH --mem=8G
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:2
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+source ~/.bashrc
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+echo "Training PredNet with default hyperparameters on KITTI dataset"
+
+python train.py
