983de67b4c7636e0c8375f4a7be7443fa4575418
jlrussin <jlrussin@gmail.com>
2019-06-20 13:29:11 -0600

    Scripts for testing time

---
+++ scripts/ecortex_test_time.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH -p local
+#SBATCH -A ecortex
+#SBATCH --mem=24G
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+echo "Training PredNet with default hyperparameters on KITTI dataset"
+
+python train.py \
+--num_iters 100 \
+--out_data_file time_test.json \
+--record_loss_every 1
