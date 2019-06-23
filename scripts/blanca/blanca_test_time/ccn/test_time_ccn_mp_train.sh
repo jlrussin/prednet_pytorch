9d94ec40585776e66901d7279dac4eda8112edf1
jlrussin <jlrussin@gmail.com>
2019-06-23 16:39:35 -0600

    Scripts for testing mp

---
+++ scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c1_b1.sh
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c2_b1.sh
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c4_b1.sh
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c8_b1.sh
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c12_b1.sh
+sbatch scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c16_b1.sh
---
+++ scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_c8_b1.sh
@@ -0,0 +1,34 @@
+##!/bin/bash
+#SBATCH --qos=blanca-ccn
+#SBATCH --mem=32G
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /pl/active/ccnlab/conda/etc/profile.d/conda.sh
+conda activate pytorch_source
+
+export MKL_NUM_THREADS=8 OMP_NUM_THREADS=8
+
+echo "MKL_NUM_THREADS: "
+echo $MKL_NUM_THREADS
+echo "OMP_NUM_THREADS: "
+echo $OMP_NUM_THREADS
+
+python train.py \
+--dataset CCN \
+--train_data_path /pl/active/ccnlab/ccn_images/wwi_emer_imgs_20fg_8tick_rot1/val/ \
+--val_data_path /pl/active/ccnlab/ccn_images/wwi_emer_imgs_20fg_8tick_rot1/val/ \
+--test_data_path /pl/active/ccnlab/ccn_images/wwi_emer_imgs_20fg_8tick_rot1/test/ \
+--seq_len 8 \
+--batch_size 1 \
+--num_iters 50 \
+--model_type PredNet \
+--results_dir ../results/train_results \
+--out_data_file blanca_test_time_ccn_train_c8_b1.json \
+--record_loss_every 1
