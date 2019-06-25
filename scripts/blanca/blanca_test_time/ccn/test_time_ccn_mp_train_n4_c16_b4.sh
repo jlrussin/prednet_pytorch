49ea070184ff9aabe7b61ad4984b7e67a45211d9
jlrussin <jlrussin@gmail.com>
2019-06-25 14:40:09 -0600

    Try nodelist

--- scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_n4_c16_b4.sh
+++ scripts/blanca/blanca_test_time/ccn/test_time_ccn_mp_train_n4_c16_b4.sh
@@ -3,6 +3,7 @@
 #SBATCH --mem=32G
 #SBATCH --time=72:00:00
 #SBATCH --nodes=4
+#SBATCH --nodelist bnode0202,bnode0203,bnode0204,bnode0205
 #SBATCH --ntasks=4
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task=16
