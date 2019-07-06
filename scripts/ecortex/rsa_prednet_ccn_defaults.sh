438bfcf38a7316eeeccd9584dc5cc2500c21e3a8
jlrussin <jlrussin@gmail.com>
2019-07-06 14:40:06 -0600

    Changed dataset to test split

--- scripts/ecortex/rsa_prednet_ccn_defaults.sh
+++ scripts/ecortex/rsa_prednet_ccn_defaults.sh
@@ -18,7 +18,7 @@ echo "Doing RSA with PredNet with default hyperparameters on CCN dataset"
 python RSA.py \
 --aggregate_method mean \
 --similarity_measure corr \
---test_data_path ../data/ccn_images/train/ \
+--test_data_path ../data/ccn_images/test/ \
 --seq_len 8 \
 --model_type PredNet \
 --load_weights_from ../model_weights/train_prednet_defaults_ccn.pt \
