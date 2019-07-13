71da44661c5deb70c47fad644fdf6e2b0c52f8a6
jlrussin <jlrussin@gmail.com>
2019-07-13 10:45:44 -0600

    retesting reduce_all op on blanca

--- scripts/blanca/blanca_test_time/ccn/test_mp_train/test_reduce/test_time_ccn_mp_train_n10_c16_b1.sh
+++ scripts/blanca/blanca_test_time/ccn/test_mp_train/test_reduce/test_time_ccn_mp_train_n10_c16_b1.sh
@@ -2,7 +2,7 @@
 #SBATCH --qos=blanca-ccn
 #SBATCH --mem=32G
 #SBATCH --time=72:00:00
-#SBATCH --nodelist=bnode[0202-0207,0211,0216,0221,0224]
+#SBATCH --nodes=10
 #SBATCH --ntasks=10
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task=16
