9e83f7df44568c91f3727871184f2ad193c5ca3e
jlrussin <jlrussin@gmail.com>
2019-07-14 13:44:43 -0600

    Script for getting mse loss of previous frame on ccn dataset

---
+++ prev_frame.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from data import *
+
+def prev_frame_loss(ccn_data_dir):
+    # CUDA
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    # Dataset
+    dataset = CCN(ccn_data_dir,8)
+    n_seqs = len(dataset)
+    batch_size = 1  # batch size is 1 for simple averaging
+    dataloader = DataLoader(dataset,batch_size,shuffle=False)
+
+    # Loss
+    mse_loss = nn.MSELoss()
+
+    losses = []
+    for X in dataloader:
+        X = X.to(device)
+        X_tm1 = X[:,:-1,:,:,:]
+        X_t = X[:,1:,:,:,:]
+        loss = mse_loss(X_tm1,X_t)
+        losses.append(loss.data.item())
+    mean_loss = np.mean(losses)
+    print("Previous frame loss for data in %s: %d" % (ccn_data_dir,mean_loss))
