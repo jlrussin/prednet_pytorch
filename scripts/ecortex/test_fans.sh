4455ebb895ea4c1cce9ae1ec163529f49b571198
jlrussin <jlrussin@gmail.com>
2019-07-22 12:23:09 -0600

    Script for testing gpu fans on ecortex cluster

---
+++ scripts/ecortex/test_fans.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#SBATCH -p local
+#SBATCH -A ecortex
+#SBATCH --qos=preemptlong
+#SBATCH --mem=32G
+#SBATCH --time=1:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+echo "Testing fans"
+
+gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+for gpu in $gpus
+do
+echo "Setting fan for" $gpu "to full"
+nvida_fancontrol full $gpu
+done
+
+sleep 10m
+
+for gpu in $gpus
+do
+echo "Setting fan for " $gpu "back to auto"
+nvida_fancontrol auto $gpu
+done
