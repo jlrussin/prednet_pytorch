82f43dfbf3230be57688f4a59f6c3c85cd90fb64
jlrussin <jlrussin@gmail.com>
2019-07-24 10:08:43 -0600

    New scripts with different learning rates for layer decoding, added lines for fan control

--- scripts/ecortex/layer_decoding_wd0p0_lrd0p0001_prednet_fc_sigmoid_tanh_lr0p0001_ccn_none.sh
+++ scripts/ecortex/layer_decoding_wd0p0_lrd0p0001_prednet_fc_sigmoid_tanh_lr0p0001_ccn_none.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -p local
 #SBATCH -A ecortex
-#SBATCH --qos=nonpreemptlong
+#SBATCH --qos=preemptlong
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --gres=gpu:1
@@ -22,27 +22,28 @@ nvidia_fancontrol full $gpu
 done
 
 echo "Doing layer decoding with PredNet with fc sigmoid tanh with lr 0.0001 on CCN dataset"
+echo "Decoder learning rate: 0.0001"
+echo "Decoder weight decay: 0.0"
 
 python layer_decoding.py \
 --aggregate_method none \
 --weight_decay 0.0 \
---load_decoders_from ../model_weights/train_prednet_fc_sigmoid_tanh_ccn_lr0p0001 \
 --train_data_path ../data/ccn_images/train/ \
 --val_data_path ../data/ccn_images/val/ \
 --test_data_path ../data/ccn_images/test/ \
 --seq_len 8 \
 --batch_size 8 \
---num_iters 50000 \
+--num_iters 100000 \
 --model_type PredNet \
 --LSTM_act sigmoid \
 --LSTM_c_act tanh \
 --bias True \
 --FC True \
 --load_weights_from ../model_weights/train_prednet_fc_sigmoid_tanh_ccn_lr0p0001.pt \
---learning_rate 0.001 \
+--learning_rate 0.0001 \
 --results_dir ../results/layer_decoding/ \
---out_data_file prednet_fc_sigmoid_tanh_lr0p0001_ccn_none.json \
---checkpoint_path ../model_weights/train_prednet_fc_sigmoid_tanh_ccn_lr0p0001 \
+--out_data_file wd0p0_lrd0p0001_prednet_fc_sigmoid_tanh_lr0p0001_ccn_none.json \
+--checkpoint_path ../model_weights/train_wd0p0_lrd0p0001_prednet_fc_sigmoid_tanh_ccn_lr0p0001 \
 --checkpoint_every 2 \
 --record_loss_every 200
 
