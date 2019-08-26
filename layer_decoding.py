6b526d1cd363b489996e7baabb757fb28b99fb74
jlrussin <jlrussin@gmail.com>
2019-08-26 13:40:29 -0700

    Layer decoding with local_grad

--- layer_decoding.py
+++ layer_decoding.py
@@ -82,6 +82,15 @@ parser.add_argument('--use_1x1_out', type=str2bool, default=False,
 parser.add_argument('--send_acts', type=str2bool, default=False,
                     help='Boolean indicating whether to send activities' +
                          'rather than errors')
+parser.add_argument('--no_ER', type=str2bool, default=False,
+                    help='Boolean indicating whether to ablate connection' +
+                         'between E_l and R_l on all but last layer')
+parser.add_argument('--RAhat', type=str2bool, default=False,
+                    help='Boolean indicating whether to add connection' +
+                         'between R_lp1 and Ahat_l')
+parser.add_argument('--local_grad', type=str2bool, default=False,
+                    help='Boolean indicating whether to restrict gradients ' +
+                         'to flow locally (within each layer)')
 # Hyperparameters for ConvLSTM
 parser.add_argument('--hidden_channels', type=int, default=192,
                     help='Number of channels in hidden states of ConvLSTM')
@@ -145,8 +154,8 @@ def main(args):
                     args.R_kernel_sizes,args.use_satlu,args.pixel_max,
                     args.Ahat_act,args.satlu_act,args.error_act,
                     args.LSTM_act,args.LSTM_c_act,args.bias,
-                    args.use_1x1_out,args.FC,args.send_acts,
-                    model_out,device)
+                    args.use_1x1_out,args.FC,args.send_acts,args.no_ER,
+                    args.RAhat,args.local_grad,model_out,device)
 
     if args.load_weights_from is not None:
         model.load_state_dict(torch.load(args.load_weights_from))
