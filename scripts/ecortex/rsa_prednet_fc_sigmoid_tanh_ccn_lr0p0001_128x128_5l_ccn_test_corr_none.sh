f35397b6148efe6e1fb2015fdfe946d3a75bfa74
jlrussin <jlrussin@gmail.com>
2019-10-09 08:18:29 -0700

    new scripts

---
+++ scripts/ecortex/rsa_prednet_fc_sigmoid_tanh_ccn_lr0p0001_128x128_5l_ccn_test_corr_none.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#SBATCH -p localLimited
+#SBATCH -A ecortex
+#SBATCH --mem=25G
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 3
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+for gpu in $gpus
+do
+echo "Setting fan for" $gpu "to full"
+nvidia_fancontrol full $gpu
+done
+
+python RSA.py \
+--aggregate_method none \
+--similarity_measure corr \
+--test_data_path ../data/ccn_images/test/ \
+--downsample_size 128 \
+--seq_len 8 \
+--idx_dict_hkl ../data/ccn_images/test_label_idx_dict.hkl \
+--model_type PredNet \
+--stack_sizes 3 32 64 128 196 \
+--R_stack_sizes 3 32 64 128 196 \
+--A_kernel_sizes 3 3 3 3 \
+--Ahat_kernel_sizes 3 3 3 3 3 \
+--R_kernel_sizes 3 3 3 3 3 \
+--LSTM_act sigmoid \
+--LSTM_c_act tanh \
+--bias True \
+--FC True \
+--load_weights_from ../model_weights/train_prednet_fc_sigmoid_tanh_ccn_lr0p0001_128x128_5l.pt \
+--results_dir ../results/rsa/ \
+--out_data_file prednet_fc_sigmoid_tanh_ccn_lr0p0001_128x128_5l.hkl
+
+
+for gpu in $gpus
+do
+echo "Setting fan for " $gpu "back to auto"
+nvidia_fancontrol auto $gpu
+done
