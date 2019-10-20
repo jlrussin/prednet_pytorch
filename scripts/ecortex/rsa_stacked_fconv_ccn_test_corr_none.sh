7b712a54a8e644c63191e13bb4489857faa75402
jlrussin <jlrussin@gmail.com>
2019-10-20 16:14:59 -0700

    new scripts

---
+++ scripts/ecortex/rsa_stacked_fconv_ccn_test_corr_none.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#SBATCH -p localLimited
+#SBATCH -A ecortex
+#SBATCH --mem=25G
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 3
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+for gpu in $gpus
+do
+echo "Setting fan for" $gpu "to full"
+nvidia_fancontrol full $gpu
+done
+
+python RSA.py \
+--aggregate_method none \
+--similarity_measure corr \
+--test_data_path ../data/ccn_images/test/ \
+--seq_len 8 \
+--idx_dict_hkl ../data/ccn_images/test_label_idx_dict.hkl \
+--model_type StackedConvLSTM \
+--R_stack_sizes 3 48 96 192 \
+--R_kernel_sizes 3 3 3 3 \
+--FC True \
+--local_grad False \
+--forward_conv True \
+--load_weights_from ../model_weights/train_stacked_fconv_lr0p0001.pt \
+--results_dir ../results/rsa/ \
+--out_data_file stacked_fconv.hkl
+
+for gpu in $gpus
+do
+echo "Setting fan for " $gpu "back to auto"
+nvidia_fancontrol auto $gpu
+done
