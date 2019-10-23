23f340d1ec7efaf120c619156b2c1095a5daad1a
jlrussin <jlrussin@gmail.com>
2019-10-22 17:13:19 -0700

    New scripts

---
+++ scripts/ecortex/train_stacked_fconv_lr0p0001_3L.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#SBATCH -p localLimited
+#SBATCH -A ecortex
+#SBATCH --nodelist=local02
+#SBATCH --mem=25G
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 3
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+for gpu in $gpus
+do
+echo "Setting fan for" $gpu "to full"
+nvidia_fancontrol full $gpu
+done
+
+python train.py \
+--dataset CCN \
+--train_data_path ../data/ccn_images/train/ \
+--val_data_path ../data/ccn_images/val/ \
+--test_data_path ../data/ccn_images/test/ \
+--seq_len 8 \
+--batch_size 8 \
+--num_iters 150000 \
+--model_type StackedConvLSTM \
+--R_stack_sizes 3 48 96 \
+--R_kernel_sizes 3 3 3 \
+--FC True \
+--local_grad False \
+--forward_conv True \
+--layer_lambdas 1.0 0.0 0.0 \
+--learning_rate 0.0001 \
+--lr_steps 0 \
+--results_dir ../results/train_results \
+--out_data_file train_stacked_fconv_lr0p0001_3L.json \
+--checkpoint_path ../model_weights/train_stacked_fconv_lr0p0001_3L.pt \
+--checkpoint_every 2 \
+--record_loss_every 200
+
+for gpu in $gpus
+do
+echo "Setting fan for " $gpu "back to auto"
+nvidia_fancontrol auto $gpu
+done
